1fe43fe6b6512e05d81955cc0a462943b168bc09
deniramadani <deni.ramadani05@gmail.com>
2022-11-15 17:57:49 +0700

    add 2 models

---
+++ community/src/main/java/com/lawencon/community/model/Product.java
@@ -0,0 +1,46 @@
+package com.lawencon.community.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import com.lawencon.base.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "tb_product")
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Product extends BaseEntity {
+
+	private static final long serialVersionUID = -6480793251581081618L;
+	@Column(nullable = false)
+	private String title;
+	@Column(nullable = false)
+	private String content;
+	@Column(nullable = false)
+	private String provider;
+	@Column(nullable = false)
+	private String location;
+	@Column(nullable = false)
+	private BigDecimal price;
+	@OneToOne
+	@JoinColumn(name = "type_product_id", nullable = false)
+	private ProductType productType;
+	@OneToOne
+	@JoinColumn(name = "owner_id", nullable = false)
+	private User ownerId;
+
+	@PrePersist
+	public void preInsert() {
+		this.price = BigDecimal.ZERO;
+	}
+
+}
---
+++ community/src/main/java/com/lawencon/community/model/Schedule.java
@@ -0,0 +1,32 @@
+package com.lawencon.community.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Table(name = "tb_schedule")
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Schedule {
+	@Column(name = "date_start", nullable = false)
+	private LocalDate dateStart;
+	@Column(name = "date_end", nullable = false)
+	private LocalDate dateEnd;
+	@Column(name = "time_start", nullable = false)
+	private LocalTime timeStart;
+	@Column(name = "time_end", nullable = false)
+	private LocalTime timeEnd;
+	@OneToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+}
