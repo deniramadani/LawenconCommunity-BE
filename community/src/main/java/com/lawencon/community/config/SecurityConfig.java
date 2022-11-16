ac96d65ea66a3bca79ffe92c3cbbe64666e54739
deniramadani <deni.ramadani05@gmail.com>
2022-11-16 11:26:45 +0700

    fix login and download file feature

--- community/src/main/java/com/lawencon/community/config/SecurityConfig.java
+++ community/src/main/java/com/lawencon/community/config/SecurityConfig.java
@@ -11,19 +11,16 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 public class SecurityConfig {
-	
-	@Bean(name="webIgnoring")
-	public List<RequestMatcher> matchers(){
+
+	@Bean(name = "webIgnoring")
+	public List<RequestMatcher> matchers() {
 		final List<RequestMatcher> matchers = new ArrayList<>();
 		matchers.add(new AntPathRequestMatcher("/swagger-ui/**", HttpMethod.GET.name()));
 		matchers.add(new AntPathRequestMatcher("/v3/**", HttpMethod.GET.name()));
 		matchers.add(new AntPathRequestMatcher("/login/**", HttpMethod.POST.name()));
 		matchers.add(new AntPathRequestMatcher("/register/**", HttpMethod.POST.name()));
 		matchers.add(new AntPathRequestMatcher("/files/**", HttpMethod.GET.name()));
-//		matchers.add(new AntPathRequestMatcher("/**", HttpMethod.POST.name()));
-//		matchers.add(new AntPathRequestMatcher("/users/**", HttpMethod.POST.name()));
-//		matchers.add(new AntPathRequestMatcher("/users/**", HttpMethod.PUT.name()));
 		return matchers;
 	}
-	
+
 }
