02ffeb404ce35fb2c736825118c522f71c723e46
deniramadani <deni.ramadani05@gmail.com>
2022-11-16 18:18:30 +0700

    set verifcode

--- community/src/main/java/com/lawencon/community/service/VerificationCodeService.java
+++ community/src/main/java/com/lawencon/community/service/VerificationCodeService.java
@@ -1,7 +1,16 @@
 package com.lawencon.community.service;
 
+import org.springframework.stereotype.Service;
+
 import com.lawencon.base.BaseCoreService;
-
+import com.lawencon.community.dto.response.ResponseDto;
+import com.lawencon.community.util.GenerateCodeUtil;
+@Service
 public class VerificationCodeService extends BaseCoreService {
-
+	public ResponseDto generateVerificationCode(String email) {
+		final String code = GenerateCodeUtil.generateCode();
+		
+		ResponseDto responseDto = new ResponseDto();
+		return responseDto;
+	}
 }
