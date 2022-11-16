cea71756ccdb973b5cdc59296765161955f40a83
deniramadani <deni.ramadani05@gmail.com>
2022-11-16 15:29:11 +0700

    add register feature

--- community/src/main/java/com/lawencon/community/service/UserService.java
+++ community/src/main/java/com/lawencon/community/service/UserService.java
@@ -10,128 +10,135 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.lawencon.base.BaseCoreService;
-import com.lawencon.base.ConnHandler;
 import com.lawencon.community.dao.IndustryDao;
 import com.lawencon.community.dao.RoleDao;
 import com.lawencon.community.dao.UserDao;
 import com.lawencon.community.dao.UserTypeDao;
+import com.lawencon.community.dto.response.ResponseDto;
 import com.lawencon.community.model.Role;
 import com.lawencon.community.model.User;
 import com.lawencon.community.model.UserType;
 import com.lawencon.config.ApiConfiguration;
 
 @Service
-public class UserService extends BaseCoreService implements UserDetailsService{
-	
-	@Autowired private UserDao userDao;
-	@Autowired private ApiConfiguration apiConfiguration;
-	@Autowired private IndustryDao industryDao;
-	@Autowired private UserTypeDao userTypeDao;
-	@Autowired private RoleDao roleDao;
-	
+public class UserService extends BaseCoreService implements UserDetailsService {
+
+	@Autowired
+	private UserDao userDao;
+	@Autowired
+	private ApiConfiguration apiConfiguration;
+	@Autowired
+	private UserTypeDao userTypeDao;
+	@Autowired
+	private RoleDao roleDao;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Optional<User> optional = userDao.getByEmail(username);
-		if(optional.isPresent()) {
-			return new org.springframework.security.core
-					.userdetails.User(username, optional.get().getPassword(), new ArrayList<>());
+		if (optional.isPresent()) {
+			return new org.springframework.security.core.userdetails.User(username, optional.get().getPassword(),
+					new ArrayList<>());
 		}
 		throw new UsernameNotFoundException("Wrong Email or Password");
 	}
-	
+
 	public Optional<User> getByEmail(final String email) {
 		return userDao.getByEmail(email);
 	}
-	
-	public User insert(final User data) {
 
-		User row = new User();
+	public ResponseDto insert(final User data) {
+		valIdNull(data);
+		valBkNotNull(data);
+		valBkNotDuplicate(data);
+		valFkFound(data);
+		final ResponseDto responseDto = new ResponseDto();
+
 		try {
 			begin();
 			final String password = apiConfiguration.passwordEncoder().encode(data.getPassword());
 			data.setPassword(password);
-			row = userDao.save(data);
+			userDao.saveNoLogin(data, ()->"4ba262b9-258b-4ae3-b879-ee286c1db783");
 			commit();
+			responseDto.setMessage("Register Success");
 		} catch (Exception e) {
 			e.printStackTrace();
 			rollback();
+			responseDto.setMessage("Register Failed");
 		}
-		return row;
+		return responseDto;
 	}
-	
-	private void valIdNotNull(final User data){
-        if(data.getId()==null) {
-            throw new RuntimeException("Primary Key Id is required!");
-        }
-    }
 
-    private void valIdExist(final User data) {
-        final User user = userDao.getByIdAndDetach(User.class, data.getId());
-        final Optional<User> optional = Optional.ofNullable(user);
-        if(optional.isEmpty()) {
-            throw new RuntimeException("Primay Key Id not found!");
-        }
-    }
+	private void valIdNotNull(final User data) {
+		if (data.getId() == null) {
+			throw new RuntimeException("Primary Key Id is required!");
+		}
+	}
 
-    private void valBkNotNull(final User data) {
-        if(data.getEmail()==null) {
-            throw new RuntimeException("BK Email is required!");
-        }
-    }
+	private void valIdExist(final User data) {
+		final User user = userDao.getByIdAndDetach(User.class, data.getId());
+		final Optional<User> optional = Optional.ofNullable(user);
+		if (optional.isEmpty()) {
+			throw new RuntimeException("Primay Key Id Is Exist!");
+		}
+	}
 
-    private void valBkNotChange(final User data) {
-        final User user = userDao.getByIdAndDetach(User.class, data.getId());
-        final Optional<User> optional = Optional.ofNullable(user);
-        if(optional.isPresent()) {
-            if(optional.get().getEmail().equalsIgnoreCase(data.getEmail())) {
-                throw new RuntimeException("BK Email cannot be changed!");
-            }
-        }
-    }
-	
-	
-	
+	private void valBkNotNull(final User data) {
+		if (data.getEmail() == null) {
+			throw new RuntimeException("BK Email is required!");
+		}
+	}
+
+	private void valBkNotChange(final User data) {
+		final User user = userDao.getByIdAndDetach(User.class, data.getId());
+		final Optional<User> optional = Optional.ofNullable(user);
+		if (optional.isPresent()) {
+			if (optional.get().getEmail().equalsIgnoreCase(data.getEmail())) {
+				throw new RuntimeException("BK Email cannot be changed!");
+			}
+		}
+	}
+
 	private void valFkFound(final User data) {
 		final UserType userType = userTypeDao.getByIdAndDetach(UserType.class, data.getUserType().getId());
 		final Optional<UserType> userTypeOptional = Optional.ofNullable(userType);
 		final Role role = roleDao.getByIdAndDetach(Role.class, data.getRole().getId());
 		final Optional<Role> roleOptional = Optional.ofNullable(role);
-		
-		if(!userTypeOptional.isPresent()) {
+
+		if (!userTypeOptional.isPresent()) {
 			throw new RuntimeException("User Type Not Found.");
 		}
-		if(!roleOptional.isPresent()) {
+		if (!roleOptional.isPresent()) {
 			throw new RuntimeException("Role Not Found.");
 		}
 	}
-	
+
 	private void valIdNull(final User data) {
-		if(data.getId() == null) {
+		if (data.getId() == null) {
 			throw new RuntimeException("Id Is Set. Expected Not Set");
 		}
 	}
-	
+
 	private void valBkNotDuplicate(final User data) {
 		final Optional<User> userOptional = userDao.getByEmail(data.getEmail());
-		if(userOptional.isPresent()) {
+		if (userOptional.isPresent()) {
 			throw new RuntimeException("Email Already Exist.");
 		}
 	}
-	
+
 	private void valNotNull(final User data) {
-		
-		if(data.getEmail() != null) {
+
+		if (data.getEmail() != null) {
 			throw new RuntimeException("Email Required.");
 		}
-		if(data.getPassword() != null) {
+		if (data.getPassword() != null) {
 			throw new RuntimeException("Password Required.");
 		}
-		if(data.getRole().getId() != null) {
+		if (data.getRole().getId() != null) {
 			throw new RuntimeException("Role Required.");
 		}
-		if(data.getUserType().getId() != null) {
-			throw new RuntimeException("User Type Required.");			
+		if (data.getUserType().getId() != null) {
+			throw new RuntimeException("User Type Required.");
 		}
 	}
-	
+
 }
