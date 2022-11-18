eb07a86376f23a6d977f03020b8db09e43ed7978
rezimaulana <rsazrm@gmail.com>
2022-11-18 10:38:18 +0700

    added dao

---
+++ community/src/main/java/com/lawencon/community/dao/PostPollingDao.java
@@ -0,0 +1,10 @@
+package com.lawencon.community.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.lawencon.base.AbstractJpaDao;
+
+@Repository
+public class PostPollingDao extends AbstractJpaDao{
+
+}
