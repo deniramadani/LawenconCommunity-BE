78eb799eb3e2d1ed4dd5632b83f1927189b21bff
deniramadani <deni.ramadani05@gmail.com>
2022-11-18 17:20:26 +0700

    get all industry AND position

--- community/src/main/java/com/lawencon/community/controller/PositionController.java
+++ community/src/main/java/com/lawencon/community/controller/PositionController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lawencon.community.model.Industry;
 import com.lawencon.community.model.Position;
-import com.lawencon.community.service.IndustryService;
 import com.lawencon.community.service.PositionService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
