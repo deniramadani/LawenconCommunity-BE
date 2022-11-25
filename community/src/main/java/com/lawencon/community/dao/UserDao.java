2c3d102c43a88d30f1dad2d01749c32ce5e1a2f6
rezimaulana <rsazrm@gmail.com>
2022-11-25 13:10:05 +0700

    get by email return user type

--- community/src/main/java/com/lawencon/community/dao/UserDao.java
+++ community/src/main/java/com/lawencon/community/dao/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.lawencon.base.AbstractJpaDao;
 import com.lawencon.community.model.Role;
 import com.lawencon.community.model.User;
+import com.lawencon.community.model.UserType;
 
 @Repository
 public class UserDao extends AbstractJpaDao {
@@ -14,9 +15,11 @@ public class UserDao extends AbstractJpaDao {
 	public Optional<User> getByEmail(final String email) {
 		final StringBuilder query = new StringBuilder()
 			.append("SELECT tb_user.id AS id, email, password, fullname, role_code, role_name, ")
+			.append("user_type_code, user_type_name, ")
 			.append("tb_user.is_active, tb_user.versions ")
 			.append("FROM tb_user ")
 			.append("INNER JOIN tb_role ON tb_user.role_id = tb_role.id ")
+			.append("INNER JOIN tb_user_type ON tb_user.user_type_id = tb_user_type.id ")
 			.append("WHERE email = :email AND tb_user.is_active = true");
 		User row = null;
 		try {
@@ -25,15 +28,19 @@ public class UserDao extends AbstractJpaDao {
 				Object[] objArr = (Object[]) userObj;
 				row = new User();
 				final Role role = new Role();
+				final UserType userType = new UserType();
 				row.setId(objArr[0].toString());
 				row.setEmail(objArr[1].toString());
 				row.setPassword(objArr[2].toString());
 				row.setFullname(objArr[3].toString());
 				role.setRoleCode(objArr[4].toString());
 				role.setRoleName(objArr[5].toString());
-				row.setIsActive(Boolean.valueOf(objArr[6].toString()));
-				row.setVersion(Integer.valueOf(objArr[7].toString()));
+				userType.setUserTypeCode(objArr[6].toString());
+				userType.setUserTypeName(objArr[7].toString());
+				row.setIsActive(Boolean.valueOf(objArr[8].toString()));
+				row.setVersion(Integer.valueOf(objArr[9].toString()));
 				row.setRole(role);
+				row.setUserType(userType);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
