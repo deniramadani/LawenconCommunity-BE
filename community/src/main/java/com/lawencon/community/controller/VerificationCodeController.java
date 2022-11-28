503ec771196c4f4e9eb986b47875d4906360bcb1
rezimaulana <rsazrm@gmail.com>
2022-11-28 10:26:35 +0700

    generate register code return 409 if exist

--- community/src/main/java/com/lawencon/community/controller/VerificationCodeController.java
+++ community/src/main/java/com/lawencon/community/controller/VerificationCodeController.java
@@ -1,5 +1,7 @@
 package com.lawencon.community.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lawencon.community.dto.response.ResponseDto;
 import com.lawencon.community.dto.verificationcode.VerificationCodeDto;
 import com.lawencon.community.model.User;
+import com.lawencon.community.service.UserService;
 import com.lawencon.community.service.VerificationCodeService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -22,12 +25,22 @@ public class VerificationCodeController {
 	
 	@Autowired
 	private VerificationCodeService verificationCodeService;
-
+	@Autowired
+	private UserService userService;
+	
 	@PostMapping("/generate")
 	public ResponseEntity<ResponseDto> generateCode(@RequestBody final User data) {
-		final ResponseDto responseDto = verificationCodeService.generateVerificationCode(data.getEmail());
-		return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
+		ResponseDto response = new ResponseDto();
+		final Optional<User> result = userService.getByEmail(data.getEmail());
+		if(result.isEmpty()) {
+			response = verificationCodeService.generateVerificationCode(data.getEmail());
+			return new ResponseEntity<ResponseDto>(response, HttpStatus.OK);			
+		} else {
+			response.setMessage("Email Already Exist!");
+			return new ResponseEntity<ResponseDto>(response, HttpStatus.CONFLICT);	
+		}
 	}
+	
 	@PostMapping("/validate")
 	public ResponseEntity<ResponseDto> validateCode(@RequestBody final VerificationCodeDto data) {
 		final ResponseDto responseDto = verificationCodeService.validateCode(data);
